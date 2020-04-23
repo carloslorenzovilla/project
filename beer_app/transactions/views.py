e8c57e175bb93516673b87e8fa6f589bf3280443
Carlos Villa <carloslorenzovilla@gmail.com>
2020-04-22 19:20:24 -0700

    refactor transactions/views.py

--- beer_app/transactions/views.py
+++ beer_app/transactions/views.py
@@ -13,9 +13,15 @@ transactions = Blueprint('transactions', __name__)
 def post_transaction():
 
     form = PostTransactionForm()
-    form.zone.choices = [(zone.id, zone.name) for zone in Zone.query.order_by('name')]
-    form.loc.choices = [(loc.id, loc.name) for loc in Location.query.order_by('name')]
-    form.item.choices=[(item.id, item.name) for item in Item.query.order_by('id')]
+    form.zone.choices = [(zone.id, zone.name) 
+                                        for zone 
+                                        in Zone.query.order_by('name')]
+    form.loc.choices = [(loc.id, loc.name) 
+                                        for loc 
+                                        in Location.query.order_by('name')]
+    form.item.choices=[(item.id, item.name) 
+                                        for item 
+                                        in Item.query.order_by('id')]
 
     if form.validate_on_submit():
         transaction = Transaction(user_id=current_user.id,
@@ -28,28 +34,18 @@ def post_transaction():
 
 @transactions.route('/loc/<zone>')
 def location(zone):
-    locations = Location.query.filter_by(zone_id=zone).all()
 
-    location_list = []
-
-    for location in locations:
-        locationObj = {}
-        locationObj['id'] = location.id
-        locationObj['name'] = location.name
-        location_list.append(locationObj)
+    location_list = [{'id':location.id,'name':location.name} 
+                                for location 
+                                in Location.query.filter_by(zone_id=zone).all()]
 
     return jsonify({'locs' : location_list})
 
 @transactions.route('/item/<loc>')
 def item(loc):
-    items = Item.query.filter_by(location_id=loc).all()
 
-    item_list = []
-
-    for item in items:
-        itemObj = {}
-        itemObj['id'] = item.id
-        itemObj['name'] = item.name
-        item_list.append(itemObj)
+    item_list = [{'id': item.id, 'name': item.name} 
+                        for item 
+                        in Item.query.filter_by(location_id=loc).all()]
 
     return jsonify({'items' : item_list})
